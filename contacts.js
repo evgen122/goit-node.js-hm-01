4ac3331ad74fcbb8b5a9afce583882e96e8d595c
evgen122 <evg.khristenko@gmail.com>
2023-12-06 23:09:18 +0200

    finish

--- contacts.js
+++ contacts.js
@@ -3,44 +3,35 @@ import fs from "fs/promises";
 import {nanoid} from "nanoid";
 import path from "path";
 
-//  Розкоментуй і запиши значення
 const contactsPath = path.resolve("db", "contacts.json");
-// console.log(contactsPath);
 
 const writeContacts = (contacts) => {
   fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
 };
 
-// TODO: задокументувати кожну функцію
 async function listContacts() {
-  // ...твій код. Повертає масив контактів.
   const data = await fs.readFile(contactsPath);
   return JSON.parse(data);
 }
 
 async function getContactById(contactId) {
-  // ...твій код. Повертає об'єкт контакту з таким id. Повертає null, якщо контакт з таким id не знайдений.
   const contacts = await listContacts();
   const result = contacts.find((item) => item.id === contactId);
   return result || null;
 }
 
 async function removeContact(contactId) {
-  // ...твій код. Повертає об'єкт видаленого контакту. Повертає null, якщо контакт з таким id не знайдений.
   const contacts = await listContacts();
   const index = contacts.findIndex((item) => item.id === contactId);
   if (index === -1) {
     return null;
   }
-  //   const deleteContact = contacts[index];
   const [result] = contacts.splice(index, 1);
-  //   await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
   await writeContacts(contacts);
   return result;
 }
 
 async function addContact(name, email, phone) {
-  // ...твій код. Повертає об'єкт доданого контакту.
   const contacts = await listContacts();
   const newContact = {
     id: nanoid(),
@@ -49,7 +40,6 @@ async function addContact(name, email, phone) {
     phone: phone,
   };
   contacts.push(newContact);
-  //   await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
   await writeContacts(contacts);
   return newContact;
 }
--- index.js
+++ index.js
@@ -9,31 +9,24 @@ import {
 
 import {program} from "commander";
 
-// const {Command} = require("commander");
-// const program = new Command();
-
 async function invokeAction({action, id, name, email, phone}) {
   switch (action) {
     case "list":
-      // ...
       const allContacts = await listContacts();
       console.table(allContacts);
       break;
 
     case "get":
-      // ... id
       const oneContact = await getContactById(id);
       console.table(oneContact);
       break;
 
     case "add":
-      // ... name email phone
       const newContact = await addContact(name, email, phone);
       console.table(newContact);
       break;
 
     case "remove":
-      // ... id
       const deleteContact = await removeContact(id);
       console.table(deleteContact);
       break;
@@ -50,13 +43,8 @@ program
   .option("-e, --email <type>", "user email")
   .option("-p, --phone <type>", "user phone");
 
-// program.parse(process.argv);
 program.parse();
 
 const argv = program.opts();
 
 invokeAction(argv);
-
-// invokeAction({ action: "read" });
-// invokeAction({action: "get", id: "drsAJ4SHPYqZeG-83QTVW"});
-// invokeAction({action: "remove", id: "drsAJ4SHPYqZeG-83QTVW"});
